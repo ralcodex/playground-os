fbc8e8fc997d49ccb4b47476ac9d1a3100813caa
frendo <Moodysnowman@outlook.com>
2026-06-12 13:38:51 -0400

    new logo (again) promise bigger commits are coming

--- m_system/conf.lua
+++ m_system/conf.lua
@@ -4,5 +4,5 @@ function love.conf(t)
     t.window.resizable = false
     t.window.fullscreen = false
     t.window.title = "playground-os pre alpha v1"
-    t.window.icon = "m_assets/oslogo.png"
+    t.window.icon = "m_assets/rainbowlogo.png"
 end
